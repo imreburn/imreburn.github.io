0651677075c1de9a1507b511c947e4c305844067
Jinhong Choi <jinhong.choi@gmail.com>
2022-10-24 10:26:57 -0700

    archive

---
+++ index.js
@@ -0,0 +1,56 @@
+/*
+Copyright 2019 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe.
+*/
+
+/* Control the default view mode */
+const viewerConfig = {
+    /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH" or "" */
+    defaultViewMode: "",
+};
+
+/* Wait for Adobe Document Services PDF Embed API to be ready */
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+    /* Initialize the AdobeDC View object */
+    var adobeDCView = new AdobeDC.View({
+        /* Pass your registered client id */
+        clientId: "d907ed00255145ff92e4d50e5468dc11",
+        /* Pass the div id in which PDF should be rendered */
+        divId: "adobe-dc-view",
+    });
+
+    /* Invoke the file preview API on Adobe DC View object */
+    adobeDCView.previewFile({
+        /* Pass information on how to access the file */
+        content: {
+            /* Location of file where it is hosted */
+            location: {
+<<<<<<< HEAD:archive/index.js
+                url: "https://www.dropbox.com/s/ejt3q35o1hbffwj/JinhongChoi_CV.pdf?dl=0"
+=======
+                url: "JinhongChoi_CV.pdf",
+>>>>>>> 35d4ad63703c00eac3a1e1d2a25e6157b4d76efa:index.js
+                /*
+                If the file URL requires some additional headers, then it can be passed as follows:-
+                headers: [
+                    {
+                        key: "<HEADER_KEY>",
+                        value: "<HEADER_VALUE>",
+                    }
+                ]
+                */
+            },
+        },
+        /* Pass meta data of file */
+        metaData: {
+            /* file name */
+            fileName: "JinhongChoi_CV.pdf"
+        }
+    }, viewerConfig);
+});
